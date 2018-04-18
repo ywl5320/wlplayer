5e86be3154eec61e36663691bb325e7b99dd1bd6
596353036@qq.com <ywl12345>
2018-04-18 17:09:40 +0800

    软解码 yuv添加滤镜

--- wlplayer/libwlplayer/src/main/res/raw/fragment_yuv.glsl
+++ wlplayer/libwlplayer/src/main/res/raw/fragment_yuv.glsl
@@ -14,5 +14,9 @@ void main() {
     rgb.g = y - 0.344 * u - 0.714 * v;
     rgb.b = y + 1.770 * u;
 
+//黑白滤镜
+//    float gray = rgb.r * 0.299 + rgb.g * 0.587 + rgb.b * 0.114;
+//    gl_FragColor = vec4(gray, gray, gray, 1);
+//原始效果
     gl_FragColor = vec4(rgb,1);
 }
