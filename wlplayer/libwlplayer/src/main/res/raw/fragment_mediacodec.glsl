6a690ff5a49524023ddafbda1702b58ddf44a02a
596353036@qq.com <ywl12345>
2018-04-18 16:58:57 +0800

    硬解码添加黑白滤镜

--- wlplayer/libwlplayer/src/main/res/raw/fragment_mediacodec.glsl
+++ wlplayer/libwlplayer/src/main/res/raw/fragment_mediacodec.glsl
@@ -4,5 +4,8 @@ varying vec2 v_texPo;
 uniform samplerExternalOES sTexture;
 
 void main() {
-    gl_FragColor=texture2D(sTexture, v_texPo);
+    //gl_FragColor=texture2D(sTexture, v_texPo);
+    lowp vec4 textureColor = texture2D(sTexture, v_texPo);
+    float gray = textureColor.r * 0.299 + textureColor.g * 0.587 + textureColor.b * 0.114;
+    gl_FragColor = vec4(gray, gray, gray, textureColor.w);
 }
